989daf39020a4291587edb7630931928019f9304
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-30 21:53:43 +0200

    account actions

--- df_account/urls.py
+++ df_account/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
+
 from registration.backends.default.views import RegistrationView
+
 from .forms import UserRegistrationForm
+
 
 """
     http://johnparsons.net/index.php/2013/06/28/creating-profiles-with-django-registration/
@@ -10,4 +13,6 @@ urlpatterns = patterns('',
                        url(r'^register/$',
                            RegistrationView.as_view(form_class=UserRegistrationForm),
                            name='registration_register'),
+
+                       (r'', include('registration.backends.default.urls')),
 )
--- df_base/views.py
+++ df_base/views.py
@@ -4,6 +4,6 @@ from df_account.forms import UserRegistrationForm
 
 def landing(request):
     if request.user.is_authenticated():
-        return HttpResponse("loggedin")
+        return render(request, 'base_logged_in.html', )
     else:
         return render(request, 'base_not_logged_in.html', dict(form_registration=UserRegistrationForm()))
