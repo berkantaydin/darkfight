d850be159bcd238378a7378e9b72efb483cd28b3
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-11-01 20:11:30 +0200

    df_char

--- df_char/urls.py
+++ df_char/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
                        url(r'profile/(?P<username>[\w-]*)$', 'df_char.views.profile', name='char_profile'),
+                       url(r'profile/use_point/(?P<field>[\w-]*)$', 'df_char.views.use_point', name='char_use_point'),
 )
--- df_char/views.py
+++ df_char/views.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.conf import settings
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from df_account.models import UserProfile
 
 
@@ -8,4 +10,29 @@ from df_account.models import UserProfile
 def profile(request, username):
     # TODO: If User exists but profile not exists status?
     profile = get_object_or_404(UserProfile, user__username=username)
+    if profile.unused_point > 0:
+        messages.info(request, 'Dağıtılmamış Yetenek Puanlarınız Var.')
     return render(request, 'df_char/profile.html', dict(profile=profile, race=settings.RACE_CHOICES))
+
+
+@login_required
+def use_point(request, field):
+    profile = get_object_or_404(UserProfile, user__username=request.user)
+    if profile.unused_point > 0:
+        if field == 'energy':
+            profile.energy_point += 1
+        if field == 'stamina':
+            profile.stamina_point += 1
+        if field == 'health':
+            profile.health_point += 1
+        if field == 'attack':
+            profile.attack_point += 1
+        if field == 'shield':
+            profile.shield_point += 1
+        if field in ('energy', 'stamina', 'health','attach','shield'):
+            profile.unused_point -= 1
+            profile.save()
+            messages.success(request, 'Puan Başarıyla Kullanıldı.')
+    else:
+        messages.error(request, 'Tüm yetenek puanlarınız kullanılmış.')
+    return redirect('char_profile', username=profile.user.username)
